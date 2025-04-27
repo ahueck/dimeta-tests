2ef89d319ef032fe60bae1f471230d79f3f11c5f
Alexander Hück <hueck.alexander@gmail.com>
2025-04-27 16:08:31 +0200

    Test pass lit setup

---
+++ testsuite/dimeta/01_malloc_int.c
@@ -0,0 +1,16 @@
+// RUN: %apply-dimeta-c %s |& %filecheck %s
+
+#include <stdlib.h>
+int main() {
+  int *test = (int *)malloc(4);
+  return 0;
+}
+
+// CHECK: SourceLoc:
+// CHECK-NEXT:   File:            01_malloc_int.c
+// CHECK-NEXT:   Function:        main
+// CHECK-NEXT:   Line:            5
+// CHECK-NEXT: Builtin:         true
+// CHECK-NEXT: Type:
+// CHECK-NEXT:   Fundamental:     { Name: int, Extent: 4, Encoding: signed_int }
+// CHECK-NEXT:   Qualifiers:      [ ptr ]
